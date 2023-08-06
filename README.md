3c51b0782f7b7882b26effbbdb80eb8949830595
Nikita Voronezhskiy <voronezhskij_n_m@sc.vsu.ru>
2023-08-06 16:27:43 +0300

    1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+# ТЗ от Neoflex.
+Spring Framework. Реализовать сервис, который будет считать отпускные.
+1. Запускаем App.
+2. Подключаемся к localhost.
+3. Вводим запрос, например: "http://localhost:8080/calculate?salary=500&vacationDays=20"
+4. Результат: Your vacation pay is RUB:27.397260273972602
--- src/main/java/ru/neoflex/neostudy/service/CalculatorVacation.java
+++ src/main/java/ru/neoflex/neostudy/service/CalculatorVacation.java
@@ -2,6 +2,7 @@ package ru.neoflex.neostudy.service;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculatorVacation {
 
     @GetMapping("/calculate")
+    @ResponseBody
     public String calculateVacation(@RequestParam double salary, @RequestParam int vacationDays) {
         double vacationPay = salary * vacationDays / 365;
         return "Your vacation pay is RUB:" + vacationPay;
